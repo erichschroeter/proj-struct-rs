03103071c9694a6a84ae9724b17a83e56655dd04
Erich Schroeter <erich.schroeter@gmail.com>
2019-04-25 20:04:00 -0400

    Added is_dir function

--- src/proj_struct.rs
+++ src/proj_struct.rs
@@ -13,6 +13,15 @@ impl Command {
     }
 }
 
+pub fn is_dir(path: &str) -> bool {
+    if path.len() > 0 {
+        if path.chars().last().unwrap() == '/' {
+            return true;
+        }
+    }
+    false
+}
+
 pub fn parse(input_string: String) -> Vec<Command> {
     let mut output: Vec<Command> = vec![];
     let lines = input_string.split("\n");
@@ -35,5 +44,15 @@ mod tests {
     fn test_parse() {
         let commands = parse("/a/path/with/multiple/directories/".to_string());
         assert_eq!(&commands[0].args[0], "/a/path/with/multiple/directories/");
+    }
+
+    #[test]
+    fn test_is_dir_returns_false_for_invalid_directory() {
+        assert!(!is_dir("example/file"));
+    }
+
+    #[test]
+    fn test_is_dir_returns_true_for_valid_directory() {
+        assert!(is_dir("example/folder/"));
     }
 }
